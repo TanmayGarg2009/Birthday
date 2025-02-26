8e7501d3ff844967ecbb9f7b39b0df86b97b7c0f
TanmayGarg2009 <tanmaypro12321@gmail.com>
2025-02-26 10:45:38 +0000

    git add effect.js git commit -m "commit message"

--- effect.js
+++ effect.js
@@ -7,11 +7,8 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-350},500);
 			$('#b22').animate({top:240, left: vw-250},500);
 			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
 			$('#b66').animate({top:240, left: vw+150},500);
 			$('#b77').animate({top:240, left: vw+250},500);
 		});
@@ -159,7 +156,7 @@ $('document').ready(function(){
 		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(4000);
-		$(this).fadeOut('fast').delay(5000).promise().done(function(){
+		$(this).fadeOut('slow').delay(5000).promise().done(function(){
 			$('#story').fadeIn('fast');
 		});
 	});
